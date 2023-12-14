8a6ab98abe7cafc77208a6ea4027b4d1fe51ae90
AiziNu <aizireknuralieva0@gmail.com>
2023-12-14 21:16:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Newcomp } from "./components/Newcomp";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Newcomp />
       </header>
     </div>
   );
---
+++ src/components/Newcomp.jsx
@@ -0,0 +1,8 @@
+export const Newcomp =()=>{
+  return (
+    <div style={{color:'red' }}>
+    <h1>Hello</h1>
+    <h2>Aizi</h2>
+    </div>
+  )
+}
